69794e5e95f16fbb6cd4d6adbb9d3389dab073f5
Ekaterina Zhmaeva <ekaterina.eduardovn@gmail.com>
2024-01-14 21:01:25 +0300

    First commit

---
+++ src/main/java/ru/netology/Post.java
@@ -0,0 +1,15 @@
+package ru.netology;
+
+public class Post {
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public Boolean subscription;
+    public FormDate birthday;
+
+    public Post() {
+        birthday = new FormDate();
+    }
+}
